78b120d3257169687f806dcdcb27851661c8631f
s0lci70 <solv.leong@gmail.com>
2023-07-03 18:55:48 -0400

    uou

--- ewe.py
+++ ewe.py
@@ -10,35 +10,16 @@ import glob
 
 prints = glob.glob("G:\\Mi unidad\\Sol\\blender\\fotogrametria\\sol\\aa\\printtest\\*.png")
 path = "E:\\Documentos\\solpy\\Nueva carpeta\\config.yml"
-# Usb(0x04b8,0x0e15,in_ep = 0x82,out_ep = 0x1,profile="TM-T20II").open
 c = config.Config()
 c.load(path)
 Epson = Usb(0x04b8,0x0e15,in_ep = 0x82,out_ep = 0x1,profile="TM-T20II")
-# Epson.control("LF")
-# Epson.control("FF")
-# Epson.control("CR")
-# Epson.control("HT")
-# Epson.control("VT")
-# print(Epson.profile.profile_data.items())
-# print(prints)
-# print(Epson.is_online())
 
 Epson.text("aa")
 
 print(Epson.profile.profile_data.items())
-Epson.text("aa")
-Epson.text(Epson.paper_status())
-
-# print(Epson)
-# preratio = 227 / 2835
-# postratio = 639 : 7980
 ratio = 2.54
 
-# print(ratio)
-# 72mm = 576 px
-# 80mm = 639 px
-# _8m = 639
-
+## RESIZE TO FULL WIDTH AND PRINT ALL IMAGES
 # for img in prints:
 #     Epson.text(img) #path
 #     Epson.cut()
@@ -48,8 +29,6 @@ ratio = 2.54
 #     Epson.image(ImgOut,center=True)
 #     Epson.cut()
     
-# print(int(ima.width*ratio))
-# c.load(path)
-# Epson.textln("AA")
+
 Epson.cut("PART")
 Epson.close()
