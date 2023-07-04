79d7041ec732aeb0412c80e29e9ca9a579efeb71
s0lci70 <solv.leong@gmail.com>
2023-07-03 21:45:16 -0400

    new images adn refactor into function

--- ewe.py
+++ ewe.py
@@ -7,28 +7,29 @@ from escpos import *
 import time
 from PIL import Image
 import glob
+from random import randint
 
-prints = glob.glob("G:\\Mi unidad\\Sol\\blender\\fotogrametria\\sol\\aa\\printtest\\*.png")
+prints = glob.glob("G:\\Mi unidad\\Sol\\blender\\fotogrametria\\sol\\aa\\printtest\\new\\*.png")
 path = "E:\\Documentos\\solpy\\Nueva carpeta\\config.yml"
 c = config.Config()
 c.load(path)
 Epson = Usb(0x04b8,0x0e15,in_ep = 0x82,out_ep = 0x1,profile="TM-T20II")
 
-Epson.text("aa")
-
 print(Epson.profile.profile_data.items())
-ratio = 2.54
+ratio = 2.5
 
-## RESIZE TO FULL WIDTH AND PRINT ALL IMAGES
-# for img in prints:
-#     Epson.text(img) #path
-#     Epson.cut()
-    
-#     imgIn = Image.open(img)
-#     ImgOut = imgIn.resize((int(imgIn.width*ratio),int(imgIn.height*ratio)))
-#     Epson.image(ImgOut,center=True)
-#     Epson.cut()
-    
+# RESIZE TO FULL WIDTH AND PRINT ALL IMAGES
+def printall():
+    for img in prints:
+        Epson.text(img)
+        Epson.cut(mode="PART")
+        
+        Epson.ln(30)
 
-Epson.cut("PART")
-Epson.close()
+        imgIn = Image.open(img)
+        ImgOut = imgIn.resize((int(576),int(imgIn.height*(ratio*1.1))))
+        Epson.image(ImgOut,center=True,impl="bitImageRaster")
+
+        Epson.ln(40)
+
+        Epson.cut()
